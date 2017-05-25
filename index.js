df043227394e23c82d414ea6583701d3e70fec80
duong van ha <friendd1996@gmail.com>
2017-05-25 15:30:04 +0700

    auto deploys

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 require('babel-core/register')({
-    presets: ['es2015', 'es2017', 'stage-0'],
-    plugins: [
-        ['transform-runtime', {
-            'polyfill'   : false,
-            'regenerator': true,
-        }],
-        'transform-decorators-legacy',
-        'transform-class-properties',
-    ],
+    presets: ['es2015'],
 });
 
 require('./server/app');
