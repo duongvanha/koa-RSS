683c05d2b73cbd03f0729813f31112ae6b79329b
duong van ha <friendd1996@gmail.com>
2017-05-25 15:50:21 +0700

    auto deploys

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 import Koa from 'koa';
 const app = new Koa();
+import path from 'path';
 
-app.use(async (ctx, next) => {
-    ctx.body = "hello word";
-});
+const serve = require('koa-static');
+
+const dir = path.join(__dirname, '..', 'public');
+
+app.use(serve(dir));
+
+// app.use(async (ctx, next) => {
+//     ctx.body = "hello word";
+// });
 
 app.listen(process.env.PORT || 8080);
