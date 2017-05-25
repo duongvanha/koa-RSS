495b4aa228502ea10b48284d64288cf020496ce1
duong van ha <friendd1996@gmail.com>
2017-05-25 14:57:59 +0700

    auto deploys

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-require('babel-core/register')({
-    presets: ['es2015', 'es2016']
-});
+// require('babel-core/register')({
+//     presets: ['es2015', 'es2016']
+// });
+//
+// require('./server/app');
 
-require('./server/app');
+
+const http = require('http');
+
+http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('okay');
+}).listen(8080);
